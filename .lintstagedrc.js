dc752e94d8dc00ccc2fdcb30b565db19ceec7b1b
Paweł Cierzniakowski <pawel@cierzniakowski.pl>
2022-06-25 00:54:11 +0200

    build: add eslint

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,8 @@
 module.exports = {
-  '*.{ts,tsx}': [() => 'tsc-files --noEmit'],
+  '*.{ts,tsx}': [
+    () => 'tsc-files --noEmit',
+    'eslint --fix',
+  ],
   '*.json': ['prettier --write'],
   '*.md': ['prettier --write --print-width 80 --prose-wrap always'],
   'yarn.lock': ['yarn-deduplicate'],
