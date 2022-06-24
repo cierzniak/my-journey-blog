ad2985622ffe24354ea432155528d20da40d0465
Paweł Cierzniakowski <pawel@cierzniakowski.pl>
2022-06-25 00:59:38 +0200

    build: add stylelint

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,6 +3,7 @@ module.exports = {
     () => 'tsc-files --noEmit',
     'eslint --fix',
   ],
+  '*.css': ['stylelint --fix'],
   '*.json': ['prettier --write'],
   '*.md': ['prettier --write --print-width 80 --prose-wrap always'],
   'yarn.lock': ['yarn-deduplicate'],
