74bdd663b4a35ce33147a66a7beb2d335a6f5753
Paweł Cierzniakowski <pawel@cierzniakowski.pl>
2022-07-01 12:34:37 +0200

    chore: add map with markers

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,14 +1,16 @@
 import { FC, useEffect, useState } from 'react';
-import Tile, { ArticleTileList, TileProps } from '../components/ArticleTile';
+import { ArticleProps } from '../components/Article';
+import Tile, { ArticleTileList } from '../components/ArticleTile';
+import Map from '../components/Map';
 
 const Index: FC = () => {
-  const [articles, setArticles] = useState<TileProps[]>([]);
+  const [articles, setArticles] = useState<ArticleProps[]>([]);
 
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
     fetch(`${process.env.REACT_APP_API_URL}/articles/1`)
       .then((res) => res.json())
-      .then((data: TileProps[]) => {
+      .then((data: ArticleProps[]) => {
         setArticles(data);
       })
       .catch(() => {});
@@ -24,6 +26,15 @@ const Index: FC = () => {
           <Tile key={slug} slug={slug} title={title} date={date} imageUrl={imageUrl} />
         ))}
       </ArticleTileList>
+      <h3>Mapa</h3>
+      <Map
+        markers={articles.map((article) => ({
+          articleSlug: article.slug,
+          city: article.location.city,
+          latitude: Number(article.location.latitude),
+          longitude: Number(article.location.longitude),
+        }))}
+      />
     </>
   );
 };
