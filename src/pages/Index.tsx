45b6009cd6ac158c6f3bac3870d03a8740b7b66b
Paweł Cierzniakowski <pawel@cierzniakowski.pl>
2022-07-01 23:08:28 +0200

    chore: loading spinner for homepage

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,8 +2,10 @@ import { FC, useEffect, useState } from 'react';
 import { ArticleProps } from '../components/Article';
 import Tile, { ArticleTileList } from '../components/ArticleTile';
 import Map from '../components/Map';
+import Loading from '../components/Loading';
 
 const Index: FC = () => {
+  const [isLoading, setLoading] = useState(true);
   const [articles, setArticles] = useState<ArticleProps[]>([]);
 
   useEffect(() => {
@@ -12,6 +14,7 @@ const Index: FC = () => {
       .then((res) => res.json())
       .then((data: ArticleProps[]) => {
         setArticles(data);
+        setLoading(false);
       })
       .catch(() => {});
   }, []);
@@ -19,22 +22,28 @@ const Index: FC = () => {
   return (
     <>
       <p>Cześć, mam na imię John i chciałbym opowiedzieć i zabrać Cię w daleką podróż.</p>
-      <p>Zaczynajmy! Wybierz pierwszą historię, którą chcesz poznać z kafelków poniżej.</p>
-      <h3>Moje ostatnie wojaże</h3>
-      <ArticleTileList>
-        {articles.map(({ slug, title, date, imageUrl }) => (
-          <Tile key={slug} slug={slug} title={title} date={date} imageUrl={imageUrl} />
-        ))}
-      </ArticleTileList>
-      <h3>Mapa</h3>
-      <Map
-        markers={articles.map((article) => ({
-          articleSlug: article.slug,
-          city: article.location.city,
-          latitude: Number(article.location.latitude),
-          longitude: Number(article.location.longitude),
-        }))}
-      />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <p>Zaczynajmy! Wybierz pierwszą historię, którą chcesz poznać z kafelków poniżej.</p>
+          <h3>Moje ostatnie wojaże</h3>
+          <ArticleTileList>
+            {articles.map(({ slug, title, date, imageUrl }) => (
+              <Tile key={slug} slug={slug} title={title} date={date} imageUrl={imageUrl} />
+            ))}
+          </ArticleTileList>
+          <h3>Mapa</h3>
+          <Map
+            markers={articles.map((article) => ({
+              articleSlug: article.slug,
+              city: article.location.city,
+              latitude: Number(article.location.latitude),
+              longitude: Number(article.location.longitude),
+            }))}
+          />
+        </>
+      )}
     </>
   );
 };
